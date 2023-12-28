3b58663829a97bbeaf7d689af66fe1d60f95b75f
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2023-12-28 16:13:48 -0300

    react

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import  Title  from './components/Title'
+import NewYear  from './assets/sl_060323_59430_23.jpg'
+import Contador from "./components/Contador"
+import useConutDown from './hooks/userCountDown'
+import './App.css'
+
+function App() {
+  const [day,hour,minute,second] = useConutDown ('jan 1, 2024 00:00:00')
+  
+
+
+  return (
+    <div className="App" style={{backgroundImage:`url(${NewYear})`}}>
+      <div className='container'>
+      <Title title= 'Contagem Regressiva' />
+      <div className='contagem regressiva'>
+        <Contador title="Dias" number={day}/>
+        <Contador title="Horas" number={hour}/>
+        <Contador title="Minutos" number={minute}/>
+        <Contador title="Segundos" number={second}/>
+      </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
